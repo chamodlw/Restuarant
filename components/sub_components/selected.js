5fc7aad23b3634d05f1e4a469c570d3ba4b3e2fb
chamodlw <chamodweerasinghe@gmail.com>
2024-09-13 22:14:12 +0530

    9/13 last

--- components/sub_components/selected.js
+++ components/sub_components/selected.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginTop: 10,
         marginHorizontal: 15,
-        borderRadius: 7,
+        borderRadius: 8,
         borderWidth: 1,
         borderColor: 'black',
         borderStyle: 'solid',
